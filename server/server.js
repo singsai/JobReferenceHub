04f9ef94fc6bc536e9ce2f87b86394bbf6367f0d
Michael Ilg <michael@ilg.ac>
2016-12-07 15:53:47 -0800

    Create dummy 'GET user' and 'POST email' express routes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-server.js
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('public'));
+app.use(express.static('dist'));
+app.use('/node_modules', express.static('node_modules'));
+
+app.get('/user', function (req, res) {
+  // GET request to get user info
+  // calling DB helper function with username to retrieve user data from DB and send it back to client
+  console.log(req.url);
+});
+
+app.post('/sendemail', function (req, res) {
+  // POST request to send reference invite email
+  // call email helper funtion with email address and email text to send email
+  console.log(req.url);
+});
+
+app.listen(3000, function () {
+  console.log('Server listening on port 3000')
+})
