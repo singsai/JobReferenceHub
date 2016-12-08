7a66a61d5245414746edbe9cc4ad60c7fcda1426
Jarob Gilliam <jarobgilliambiz@gmail.com>
2016-12-07 17:54:46 -0800

    Removed helper functions. (Commented them out.) Determined that they were unnecessary. Mongoose's query functions are more useable than any helper function I could write in a reasonable time.

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,11 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/reference');
 
-// Define our connection object.
-var db = mongoose.connection();
-db.once('open', function() {
-  // connected
-});
+console.log('Mongoose is:', mongoose);
+
 
 ///////////////////////////// SCHEMA DEFINITIONS ////////////////////////////////
 var userSchema = mongoose.Schema({
