f64984f408a6ef97bfdaf7eaa38009cbde2bc60a
Michael Ilg <michael@ilg.ac>
2016-12-07 19:08:33 -0800

    Turn DB helpers to request handlers

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 var express = require('express');
-var email = require('./email.js');
+// var email = require('./email.js');
+var handler = require('../request-handler.js');
 
 var app = express();
 
@@ -8,38 +8,21 @@ app.use(express.static('public'));
 app.use(express.static('dist'));
 app.use('/node_modules', express.static('node_modules'));
 
-//fake user data for testing
-
-var fakeUserData = {
-  username: 'Kai',
-  email: 'kaitheboss@aol.com',
-  references: [{stars: 5, text: 'Kai is the cuttest'}, {stars: 2, text: 'worked with Kai, was soso'}]
-};
-
-
-
 //routes
+app.post('/user', handler.addUser);
 
-app.get('/user', function (req, res) {
-  // GET request to get user info
-  // calling DB helper function with username to retrieve user data from DB and send it back to client
-  console.log(req.url);
-  res.send(fakeUserData);
-});
+app.get('/allusers', handler.findAllUser);
 
-app.post('/sendemail', function (req, res) {
-  // POST request to send reference invite email
-  // call email helper funtion with email address and email text to send email
-  console.log(req.url);
-  res.sendStatus(201);
-});
+app.get('/user', handler.findUser);
 
-app.post('/postreference', function (req, res) {
-  // POST request to write references for outher useers
-  // call DB helper function with reference text to store reference in DB under according user
-  // something like:
-  // DBhelper.createReference(fakeUserData.username, fakeUserData.references[0], callback? )
-  res.sendStatus(201);
+app.post('/addreference',handler.addReference);
+
+app.get('/allreferences', handler.findAllReferences);
+
+app.get('/reference', handler.findReference);
+
+app.post('/sendemail', handler.sendInvite);
+
 })
 
 app.listen(3000, function () {
