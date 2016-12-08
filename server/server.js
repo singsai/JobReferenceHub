ad38a64453e68fe9c70db4bbbbf10ebb65e5b336
Michael Ilg <michael@ilg.ac>
2016-12-07 16:54:21 -0800

    Integrate email module in server and add 'POST reference' route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var email = require('./email.js');
 
 var app = express();
 
 app.use(express.static('public'));
 app.use(express.static('dist'));
 app.use('/node_modules', express.static('node_modules'));
+
+//routes
 
 app.get('/user', function (req, res) {
   // GET request to get user info
@@ -17,6 +20,11 @@ app.post('/sendemail', function (req, res) {
   // call email helper funtion with email address and email text to send email
   console.log(req.url);
 });
+
+app.post('/postreference', function (req, res) {
+  // POST request to write references for outher useers
+  // call DB helper function with reference text to store reference in DB under according user 
+})
 
 app.listen(3000, function () {
   console.log('Server listening on port 3000')
