c8b4ce55026e5aefe2527d309302376131039b56
Michael Ilg <michael@ilg.ac>
2016-12-07 17:11:40 -0800

    Send fakeData back to GET requests and to DB helper for testing

--- server/server.js
+++ server/server.js
@@ -7,23 +7,38 @@ app.use(express.static('public'));
 app.use(express.static('dist'));
 app.use('/node_modules', express.static('node_modules'));
 
+//fake user data for testing
+
+var fakeUserData = {
+  username: 'Kai',
+  email: 'kaitheboss@aol.com',
+  references: [{stars: 5, text: 'Kai is the cuttest'}, {stars: 2, text: 'worked with Kai, was soso'}]
+};
+
+
+
 //routes
 
 app.get('/user', function (req, res) {
   // GET request to get user info
   // calling DB helper function with username to retrieve user data from DB and send it back to client
   console.log(req.url);
+  res.send(fakeUserData);
 });
 
 app.post('/sendemail', function (req, res) {
   // POST request to send reference invite email
   // call email helper funtion with email address and email text to send email
   console.log(req.url);
+  res.sendStatus(201);
 });
 
 app.post('/postreference', function (req, res) {
   // POST request to write references for outher useers
-  // call DB helper function with reference text to store reference in DB under according user 
+  // call DB helper function with reference text to store reference in DB under according user
+  // something like:
+  // DBhelper.createReference(fakeUserData.username, fakeUserData.references[0], callback? )
+  res.sendStatus(201);
 })
 
 app.listen(3000, function () {
