235c5faf800d30f87373bbb6ac9fbb6a7db76b2a
Kai Hsia <kaihsia@gmail.com>
2016-12-07 18:54:44 -0800

    attempted implementing passport

--- server/server.js
+++ server/server.js
@@ -2,31 +2,55 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var express = require('express');
-var email = require('./email.js');
+// var email = require('./email.js');
 
 var app = express();
 
+app.use(require('express-session')({
+    secret: 'ambitious-elm',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static('public'));
 app.use(express.static('dist'));
 app.use('/node_modules', express.static('node_modules'));
 
-//fake user data for testing
+// auth configuration
+var account = require('../db/database-config');
+passport.use(new LocalStrategy(account.User.authenticate()));
+passport.serializeUser(account.User.serializeUser());
+passport.deserializeUser(account.User.deserializeUser());
 
+
+//fake user data for testing
 var fakeUserData = {
   username: 'Kai',
   email: 'kaitheboss@aol.com',
   references: [{stars: 5, text: 'Kai is the cuttest'}, {stars: 2, text: 'worked with Kai, was soso'}]
 };
 
-
-
 //routes
 
 app.get('/user', function (req, res) {
   // GET request to get user info
   // calling DB helper function with username to retrieve user data from DB and send it back to client
+
   console.log(req.url);
   res.send(fakeUserData);
+});
+
+app.post('/register', function (req, res) {
+  account.user.register(new account.user({ username : req.body.username }), req.body.password, function(err, account) {
+    if (err) {
+        return res.render('register', { account : account });
+    }
+
+    passport.authenticate('local')(req, res, function () {
+        res.redirect('/sendmail');
+    });
+  });
 });
 
 app.post('/sendemail', function (req, res) {
