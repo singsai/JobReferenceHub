1c9fba1f58599a94b2f1eb365bfe7a743c4c2422
Jarob Gilliam <jarobgilliambiz@gmail.com>
2016-12-11 21:23:06 -0800

    minor changes

--- src/Profile.js
+++ src/Profile.js
@@ -13,35 +13,19 @@ class Profile extends React.Component {
 	constructor(props) {
     super(props);
     this.state = {
-      currentUser: mockData
+      currentUser: mockData,
+      references: null
     };
     // this.createUser();
     this.getUserData('bob');
   }
 
   getUserData(username) {
-
     $.get('/user/' + username, function(data) {
-      console.log('Data is:', data[0]);
       this.setState({
         currentUser: data[0]
       });
-      console.log('This.state:', this.state);
     }.bind(this));
-
-
-
-    // fetch('/user/' + username)
-    // .then(function(data) {
-    //   return data.blob();
-    // })
-    // .then(function(blobData) {
-    //   console.log('Data definitely is:', blobData);
-    //   this.setState({
-    //     currentUser: blobData
-    //   });
-    //   console.log('This.state:', this.state);
-    // }.bind(this))
   }
 
   createUser() {
@@ -58,6 +42,7 @@ class Profile extends React.Component {
         Current Company: {this.state.currentUser.profileInfo.currentCompany} <br/>
         Job: {this.state.currentUser.profileInfo.role} <br/>
       </div>
+      
     )
   }
 }
